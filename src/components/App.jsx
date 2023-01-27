1c9d2c0590895a492d3554d6511caf85550fd4e3
Svetlana210 <svetik.may24@gmail.com>
2023-01-27 09:57:53 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,19 @@ import Filter from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('my-contacts'));
+    this.setState({ contacts });
+  }
+
+  componentDidUpdate() {
+    const { contacts } = this.state;
+    localStorage.setItem('my-contacts', JSON.stringify(contacts));
+  }
 
   formSubmitHandler = data => {
     const id = nanoid();
